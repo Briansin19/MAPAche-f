1e2b888c53db26b65e8689771986e2e8729af8c1
Emiliano Reyes <calvin_2111@hotmail.com>
2025-01-06 15:24:10 -0500

    class Building deleted

--- app/src/main/java/com/example/mapache_f/classes/Building.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mapache_f.classes
-
-data class Building(
-    var id: String = "",
-    var name: String = "",
-    var description: String = "",
-    var latitude: Double = 0.0,
-    var longitude: Double = 0.0
-)
