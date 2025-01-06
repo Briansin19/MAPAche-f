42ff8a88374d625305159a3604f63e39cbdf5e60
Briansin19 <holaqhacebrian098@gmail.com>
2025-01-05 22:30:15 -0500

    updates

--- app/src/main/java/com/example/mapache_f/screens/map/BuildingApiService.kt
+++ app/src/main/java/com/example/mapache_f/screens/map/BuildingApiService.kt
@@ -3,7 +3,7 @@ package com.example.mapache_f.screens.map
 import retrofit2.Call
 import retrofit2.http.GET
 
-data class Building(val name: String, val lat: Double, val lng: Double)
+data class Building(val id: String, val name: String, val lat: Double, val lng: Double)
 
 interface BuildingApiService {
     @GET("buildings")
